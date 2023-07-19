a7f8d3dfec23b1c636db8bcfbcf6fb63d90513d6
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2023-07-19 15:08:19 +0200

    working with hardcoded texture

--- src/Components/Artwork.jsx
+++ src/Components/Artwork.jsx
@@ -6,63 +6,60 @@ import { suspend } from "suspend-react";
 import { Suspense } from "react";
 
 
- function getRandomArtwork() {
-     let artworkData = null;
-     let imageId = '';
-     let randomId = 0;
-     let errorCount = 0
+async function getRandomArtwork(setArtwork, setError) {
+    let artworkData = null;
+    let imageId = '';
+    let randomId = 0;
+    let errorCount = 0
 
-     while (!artworkData && errorCount < 10) {
-         randomId = Math.floor(Math.random() * 100000);
+    while (!artworkData && errorCount < 10) {
+        randomId = Math.floor(Math.random() * 100000);
+        try {
+            const response = await fetch(`https://api.artic.edu/api/v1/artworks/${randomId}?fields=id,title,image_id,artist_display`);
+            if (response.ok) {
+                artworkData = await response.json();
+                imageId = artworkData.data.image_id;
+            } else {
+                errorCount++;
+            }
+        } catch (error) {
+            errorCount++;
+        }
+    }
 
-         const data = suspend(async () => {
-             const res = await fetch(`https://api.artic.edu/api/v1/artworks/${randomId}?fields=id,title,image_id,artist_display`);
-             if (res.ok) {
-                 artworkData = res.json();
-                 imageId = artworkData.data.image_id;
-                 console.log(imageId)
-             } else {
-                 errorCount++;
-             }
-         })
+    if (artworkData) {
+        setArtwork({
+            ...artworkData.data,
+            imageUrl: `https://www.artic.edu/iiif/2/${imageId}/full/403,/0/default.jpg`
+        });
+    } else {
+        setError(new Error('Impossible de récupérer une œuvre d\'art.'));
+    }
+}
 
+export default function Artwork()
+{
+    const texture = useTexture('https://www.artic.edu/iiif/2/743f53b8-eb8b-4d12-3031-20e2c31c1f26/full/403,/0/default.jpg')
+    const [artwork, setArtwork] = useState(null);
+    const [error, setError] = useState(null);
 
-         if (artworkData) {
-             setArtwork({
-                 ...artworkData.data,
-                 imageUrl: `https://www.artic.edu/iiif/2/${imageId}/full/403,/0/default.jpg`
-             });
-         } else {
-             setError(new Error('Impossible de récupérer une œuvre d\'art.'));
-         }
-     }
- }
- getRandomArtwork()
+    useEffect(() => {
+        getRandomArtwork(setArtwork, setError);
+    }, []);
 
-export default function Artwork(){}
-// {
-//     const texture = useTexture('https://www.artic.edu/iiif/2/743f53b8-eb8b-4d12-3031-20e2c31c1f26/full/403,/0/default.jpg')
-//     // const texture = useTexture(`https://www.artic.edu/iiif/2/${imageId}/full/403,/0/default.jpg`)
-//     const [artwork, setArtwork] = useState(null);
-//     const [error, setError] = useState(null);
-//     let texture2 = useTexture('')
-//
-//     // useEffect(() => {
-//     //     getRandomArtwork(setArtwork, setError);
-//     // }, []);
-//     //
-//     // if(artwork){
-//     //     console.log(artwork.imageUrl)
-//     //     texture2 = useTexture(artwork.imageUrl)
-//     // }
-//
-//     return (<>
-//             {/*<Suspense fallback={<div>loading...</div>}>*/}
-//                 <mesh>
-//                     <planeGeometry args={ [15,15] } />
-//                     <meshStandardMaterial map={texture} />
-//                 </mesh>
-//             {/*</Suspense>*/}
-//         </>
-//     )
-// }
+    if(artwork){
+
+        // console.log(artwork.imageUrl)
+        // texture2 = useTexture(artwork.imageUrl)
+    }
+
+    return (<>
+            <mesh>
+                <planeGeometry args={ [15,15] } />
+                <meshStandardMaterial map={texture} />
+                {/*<meshStandardMaterial map={artwork.imageUrl} />*/}
+            </mesh>
+        </>
+
+    )
+}
