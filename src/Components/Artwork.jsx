44a3ad928bcec7942e2f9893055858a330abdb0e
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2023-07-20 23:28:27 +0200

    dynamic artWork

--- src/Components/Artwork.jsx
+++ src/Components/Artwork.jsx
@@ -1,7 +1,5 @@
 import { DoubleSide } from 'three';
-import {Center, useTexture} from '@react-three/drei';
-import {useControls} from "leva";
-import {useEffect, useState} from "react";
+import {Box, Center, useTexture} from '@react-three/drei';
 import { suspend } from "suspend-react";
 import { Suspense } from "react";
 
@@ -10,23 +8,19 @@ async function getRandomArtwork() {
     let randomId = 0;
     let errorCount = 0;
     let artWorkFound = false;
+    console.log(artWorkFound)
 
     while (!artWorkFound) {
         randomId = Math.floor(Math.random() * 100000);
         const response = await fetch(`https://api.artic.edu/api/v1/artworks/${randomId}?fields=id,title,image_id,artist_display`);
 
         if (response.ok) {
-            console.log('res ok');
             artWorkFound = true;
             artworkData = await response.json();
-            let imageId = artworkData.data.image_id;
-            console.log(imageId);
         } else {
-            console.log("res pas ok");
             errorCount++;
         }
     }
-
     return artworkData;
 }
 
@@ -44,24 +38,25 @@ function GetRandomArtwork() {
     const artwork = ArtworkData.data;
     const imageUrl = useTexture(`https://www.artic.edu/iiif/2/${artwork.image_id}/full/403,/0/default.jpg`)
 
-    console.log(imageUrl)
 
-    return (
+    return (<>
         <mesh>
             <planeGeometry args={[15, 15]} />
             <meshStandardMaterial map={imageUrl} />
         </mesh>
+            <Box args={[10,10,1]} material-color="white" />
+        </>
     );
+
 }
 
+
+
 export default function Artwork() {
+
     return (
         <Suspense fallback={console.log("waiting .....")}>
             <GetRandomArtwork />
-        </Suspense>,
-
-    <Suspense fallback={console.log("waiting .....")}>
-        <GetRandomArtwork />
-    </Suspense>
+        </Suspense>
     );
 }
