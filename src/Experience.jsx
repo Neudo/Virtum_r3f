81bb5450353e98d8ed5bbc11a90e0a04d6123ddd
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2023-08-02 20:13:59 +0200

    Add reveal effect

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -18,23 +18,30 @@ import {DEG2RAD} from "three/src/math/MathUtils.js";
 
 export default function Experience()
 {
-    const directionalLight = useRef()
-    useHelper(directionalLight, THREE.DirectionalLightHelper, 15)
+    const spotLight = useRef()
     const { Lights } = useControls('Spot light Right', {
         Lights: { value: [4, 60, 7] }
     })
 
-    const spotLight = useRef()
     useHelper(spotLight, THREE.SpotLightHelper, "blue")
 
     const spotLightLeft = useRef()
     useHelper(spotLightLeft, THREE.SpotLightHelper, 15)
 
+
     const { LightLeft } = useControls('Spot light left', {
         LightLeft: { value: [11, 60, 6] }
     })
 
-    const [matcapTextureSol] = useMatcapTexture('C2AB7D_4A412E_7A6B4E_F9EDBE\n', 1024)
+    const { position } = useControls('Text position', {
+        position: { value: [-1,50,20]},
+    })
+    const {rotationX } = useControls('Text rotation', {
+        rotationX: {value: -.88, step: .01}
+    })
+
+
+
 
     const groupeRef = useRef()
 
@@ -67,6 +74,10 @@ export default function Experience()
         setTimeout(() => {
             mask.classList.add('hidden')
         }, 1000);
+
+        setTimeout(() => {
+           startBtn.style.opacity = 1
+        }, 3000);
         let experienceStarted = false
 
         startBtn.addEventListener('click', () =>
@@ -78,7 +89,7 @@ export default function Experience()
                 cameraControlsRef.current?.rotate(0, 25 * DEG2RAD, true)
                 setTimeout(() => {
                     cameraControlsRef.current?.rotate(0, 55 * DEG2RAD, true);
-                }, 1300);
+                }, 1100);
 
             }
         })
@@ -134,14 +145,13 @@ export default function Experience()
         <Plane
             receiveShadow position-y={ - 15 } rotation-x={ - Math.PI * 0.5 } scale={ 20 } args={[10, 10, 128,128]}  >
             <meshStandardMaterial color={'black'}/>
-            {/*<MeshReflectorMaterial resolution={512} blur={[1000, 1000]} mixBlur={1} mirror={.2} color="whitesmoke"  />*/}
         </Plane>
 
         <Text
             color="white"
             fontSize={4}
-            position={[0,50,20]}
-            rotation-x={Math.PI * -.32}
+            position={position}
+            rotation-x={rotationX}
         >
             Virtum
         </Text>
