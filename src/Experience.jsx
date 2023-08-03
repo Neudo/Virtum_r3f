dc854831c646490e3c880921da954769c831f11f
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2023-08-03 12:14:15 +0200

    remvoe media query

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -111,7 +111,7 @@ export default function Experience()
         setEnabled(true)
     }
 
-    const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+    // const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
 
     return <>
         <CameraControls
@@ -166,7 +166,7 @@ export default function Experience()
 
         <Text
             color="white"
-            fontSize={isMobile ? 2 : 4}
+            fontSize={2}
             position={position}
             rotation-x={rotationX}
         >
